09c571a2646bd6d98c81efad475cc76f86bd921e
Andrea Guzzo <xant@xant.net>
2015-03-26 12:16:20 +0100

    fixed some bugs introduced with recent changes

--- src/shardcache.c
+++ src/shardcache.c
@@ -1138,7 +1138,7 @@ shardcache_get_helper(void *key,
         return -1;
     }
 
-    if (total_size) {
+    if (timestamp) {
         ATOMIC_SET(arg->complete, 1);
         if (timestamp)
             memcpy(&arg->ts, timestamp, sizeof(struct timeval));
