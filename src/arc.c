c69e4f609edbc5c7524fc1fffde38d312a6a0623
Andrea Guzzo <xant@xant.net>
2015-03-27 12:43:58 +0100

    fixed a memory leak

--- src/arc.c
+++ src/arc.c
@@ -601,10 +601,10 @@ int arc_lookup_multi(arc_t *cache,
                      time_t ttl)
 {
     int i;
-    int *missing = NULL;
-    int missing_count = 0;
 
     if (cache->ops->fetch_multi) {
+        int *missing = NULL;
+        int missing_count = 0;
         for (i = 0; i < num_keys; i++) {
             resources[i] = arc_lookup_nofetch(cache, keys[i], klens[i], NULL);
             if (!resources[i]) {
@@ -626,6 +626,7 @@ int arc_lookup_multi(arc_t *cache,
                 if (UNLIKELY(!obj)) {
                     //TODO - handle error and release resources
                     free(mem);
+                    free(missing);
                     return -1;
                 }
 
@@ -664,6 +665,7 @@ int arc_lookup_multi(arc_t *cache,
             {
                 // TODO - Handle error
                 free(mem);
+                free(missing);
                 return -1;
             }
             
@@ -681,6 +683,7 @@ int arc_lookup_multi(arc_t *cache,
             }
 
             free(mem);
+            free(missing);
         }
     } else {
         for (i = 0; i < num_keys; i++) {
